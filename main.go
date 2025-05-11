799419c03f979d2b94fc118425d3c450bbaa3941
rahul yadav <rahulyadav221007@gmail.com>
2025-05-11 16:30:59 +0530

    initial setup

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	"golang-restrohub/database"
+
+	middleware "golang-restrohub/middleware"
+	routes "golang-restrohub/routes"
+
+	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
+
+}
